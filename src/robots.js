3bd92ab4eb57ca5dadda54e775b76f861b2b8c59
Ken Charette <charettekenneth@gmail.com>
2018-07-24 15:56:02 -0400

    adding robofriends React app

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Ken Charette',
+    username: 'Koala Trust',
+    email: 'koalatruste@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Irvine Peppercorn',
+    username: 'Moxy',
+    email: 'moxytv@hotmail.com'
+  },
+  {
+    id: 3,
+    name: 'Clementine X',
+    username: 'Raptor',
+    email: 'clevergirl@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Patricia Jungle',
+    username: 'KnivesX',
+    email: 'pat.jungle@idk.com'
+  },
+  {
+    id: 5,
+    name: 'Baelor 60 Priest',
+    username: 'bangingbaelor',
+    email: 'baelorisreal@omg.what'
+  },
+  {
+    id: 6,
+    name: 'Wrandon Bieber',
+    username: 'Exiled Coffin',
+    email: 'Talking_Big@bigly.com'
+  },
+  {
+    id: 7,
+    name: 'Berrnsteiny ',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Daniel Phalanx',
+    username: 'Delphine',
+    email: 'MitchConnors@dana.org'
+  },
+  {
+    id: 10,
+    name: 'Canelatina Francois',
+    username: 'The High Rad',
+    email: 'small_opinion@skies.biz'
+  }
+];
